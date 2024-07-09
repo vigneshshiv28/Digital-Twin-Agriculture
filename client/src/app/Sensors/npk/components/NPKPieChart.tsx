d3c3b47a41b6d5fabc918be2f4575aa658b4cd7f
Vignesh <vigneshshivhare28@gmail.com>
2024-07-09 19:57:27 +0530

    Sensor page completed

---
+++ client/src/app/Sensors/npk/components/NPKPieChart.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    PieChart,
+    Pie,
+    Legend,
+    Cell,
+    ResponsiveContainer,
+    Label
+  } from "recharts";
+
+function NPKPieChart() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NPKPieChart
